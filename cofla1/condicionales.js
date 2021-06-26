ee7458eee4ce5e16a48ad54411b1a3edcd187e7f
juano9228 <thejuano9228@gmail.com>
2021-06-26 12:12:22 -0500

    working on testout and condicionales.js

--- cofla1/condicionales.js
+++ cofla1/condicionales.js
@@ -1,1 +1,16 @@
-alert("Examples about if");
+//true or false
+//*
+let admitido = 2;
+
+if (admitido == true) {
+    alert("Valor admitido");
+
+} else if (admitido == false) {
+    document.write("Valor no admitido")
+}
+
+else{
+    document.write("NO ES UN VALOR DEFINIDO")
+}
+/*/
+
