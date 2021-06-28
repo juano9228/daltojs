c9eb8f3ccdfce4c947f3fec27c92969ce078e828
extinto90w <extintofrom90s@gmail.com>
2021-06-27 22:13:16 -0500

    Lazy sunday 27-06-21

--- cofla1/condicionales.js
+++ cofla1/condicionales.js
@@ -15,9 +15,22 @@ else{
 */
 
 //------VALIDATION--------------------------------------------------------
-let nombre, edad;
+/*let nombre, edad;
 nombre = prompt("Introducue tu nombre");
 if (nombre == "") {
     alert("Nombre no introducido!!");
 }
+else{
+    alert('Hola '+nombre + '. Biemvenido a esta pagina web');
+}
+edad = prompt ('Cual es tu edad');
+edad = Number(edad);
 
+if (edad <= 17) {
+    alert("Eres menor de edad");
+}
+else if (edad > 18){
+    
+    alert("Eres mayor de edad");
+}
+*/
