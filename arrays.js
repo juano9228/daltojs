4504bb8a680064c83d6661e1384cce53959d5bcd
extinto90w <extintofrom90s@gmail.com>
2021-06-29 20:53:20 -0500

    Extinto90H add cofla2

---
+++ arrays.js
@@ -0,0 +1,14 @@
+alert("Hola mundo")
+ /*frutas = ["bananas", "manzana", "pera"];
+
+ document.write(frutas[2])
+
+ /////////////////////////////////////////////////
+ let pc = {
+    nombre: "DaltoPC",
+    procesador: "Intel",
+    ram: "16GB",
+    espacio: "1TB"
+ };
+
+ document.write(pc["nombre"])*/
---
+++ cofla2/problema2.js
@@ -0,0 +1,1 @@
+alert("Problema2");
