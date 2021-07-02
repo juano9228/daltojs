c4ea4e285a1db9e251f148784e43aa1629be0eca
juano9228 <thejuano9228@gmail.com>
2021-07-02 13:04:34 -0500

    adding cofla6

---
+++ cofla6/El DOM.js
@@ -0,0 +1,7 @@
+alert("EL DOM");
+/*Nodo: un nodo es en el DOM cualquier 
+etiqueta del cuerpo, como un parrrafo, el mismo body o incluso las etiquetas
+de una lista, los nodos no siempre  suelen ser etiquetas
+
+
+*/
